776c69093f5cb73fedceacc72bb5126e84eb7554
Romoshi <derinrom@gmail.com>
2024-04-15 18:43:58 +0500

    refact update amount method

--- src/main/java/test/task/romoshi/javacode/service/WalletService.java
+++ src/main/java/test/task/romoshi/javacode/service/WalletService.java
@@ -1,13 +1,14 @@
 package test.task.romoshi.javacode.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.util.concurrent.AtomicDouble;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import test.task.romoshi.javacode.entity.OperationType;
 import test.task.romoshi.javacode.entity.Wallet;
 import test.task.romoshi.javacode.entity.WalletRequest;
 import test.task.romoshi.javacode.repository.WalletRepository;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,22 +17,32 @@ public class WalletService {
     private final WalletRepository walletRepository;
 
     public Wallet getWalletById(String walletId) {
-        return walletRepository.findById(walletId).orElseThrow();
+        return walletRepository.findById(walletId).orElseThrow(NoSuchElementException::new);
     }
 
     public void updateWalletAmount(WalletRequest request) {
-        Wallet wallet = walletRepository.findById(request.getWalletId()).orElseThrow();
+        Wallet wallet = walletRepository.findById(request.getWalletId()).orElseThrow(NoSuchElementException::new);
+        AtomicDouble atomicAmount = new AtomicDouble(wallet.getC_amount());
+        double currentAmount;
+        double newAmount;
 
         if(request.getOperationType() == OperationType.DEPOSIT) {
-            wallet.setC_amount(wallet.getC_amount() + request.getAmount());
+            do {
+                currentAmount = atomicAmount.get();
+                newAmount = currentAmount + request.getAmount();
+            } while (!atomicAmount.compareAndSet(currentAmount, newAmount));
         }
+
         if(request.getOperationType() == OperationType.WITHDRAW) {
-            double withdrawDif = wallet.getC_amount() - request.getAmount();
-            if(withdrawDif <= 0) {
-                throw new IllegalArgumentException();
-            } else {
-                wallet.setC_amount(withdrawDif);
-            }
+            do {
+                currentAmount = atomicAmount.get();
+                newAmount = currentAmount - request.getAmount();
+                if(newAmount <= 0) {
+                    throw new IllegalArgumentException();
+                } else {
+                    wallet.setC_amount(newAmount);
+                }
+            } while (!atomicAmount.compareAndSet(currentAmount, newAmount));
         }
         walletRepository.save(wallet);
     }
